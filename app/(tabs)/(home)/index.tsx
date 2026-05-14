dcee110225e72c8cb84c3c40707d084be19f7c2f
Sourav Banerjee <sourav.banerjeejobs@gmail.com>
2026-05-14 21:02:49 +0530

    working on homepage screen, continue watching movie card, unfinished

--- app/(tabs)/(home)/index.tsx
+++ app/(tabs)/(home)/index.tsx
@@ -1,12 +1,59 @@
+import ContinueWatchingMovieCard from "@/components/ContinueWatchingMovieCard";
+import MovieCard from "@/components/MovieCard";
 import OverviewSection from "@/components/OverviewSection";
 import Colors from "@/constants/colors";
-import { ScrollView, StyleSheet, View } from "react-native";
+import { movies } from "@/constants/mock-data";
+import {
+  FlatList,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+
+const SectionHeader = ({ title }: { title: string }) => {
+  return (
+    <View style={styles.sectionHeader}>
+      <Text style={{ color: Colors.text, fontWeight: "600", fontSize: 16 }}>
+        {title}
+      </Text>
+      <TouchableOpacity activeOpacity={0.8} style={{ padding: 10 }}>
+        <Text style={{ color: Colors.primary }}>See more</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
 
 export default function HomeScreen() {
   return (
     <View style={styles.container}>
       <ScrollView>
         <OverviewSection />
+        <View style={{ flex: 1 }}>
+          <View style={{ marginVertical: 20 }}>
+            <SectionHeader title="Trending now 🔥" />
+          </View>
+          <FlatList
+            data={movies}
+            renderItem={({ item }) => (
+              <MovieCard
+                image={item.image}
+                title={item.title}
+                genre={item.genre}
+              />
+            )}
+            horizontal
+          />
+        </View>
+        <View style={{ marginVertical: 20 }}>
+          <SectionHeader title="Continue Watching" />
+          <ContinueWatchingMovieCard
+            title="The Sandman"
+            image={require("@/assets/images/movie1.png")}
+            genre="Action | Drama"
+          />
+        </View>
       </ScrollView>
     </View>
   );
@@ -16,5 +63,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: Colors.background,
+  },
+  sectionHeader: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginBottom: 14,
+    paddingHorizontal: 14,
   },
 });
---
+++ components/MovieCard.tsx
@@ -0,0 +1,53 @@
+import Colors from "@/constants/colors";
+import { hs, ms, vs } from "@/constants/screen-dimensions";
+import {
+  Image,
+  ImageSourcePropType,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+
+interface MovieCardProps {
+  image?: ImageSourcePropType | undefined;
+  title: string;
+  genre: string;
+}
+
+const MovieCard = ({ image, title, genre }: MovieCardProps) => {
+  return (
+    <TouchableOpacity activeOpacity={0.8} style={styles.container}>
+      <Image source={image} style={styles.image} />
+      <View>
+        <Text
+          numberOfLines={1}
+          style={{ color: Colors.text, fontWeight: "600", fontSize: ms(14) }}
+        >
+          {title}
+        </Text>
+        <Text
+          numberOfLines={1}
+          style={{ color: Colors.gray, fontSize: ms(10) }}
+        >
+          {genre}
+        </Text>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+export default MovieCard;
+
+const styles = StyleSheet.create({
+  container: {
+    width: hs(110),
+    marginHorizontal: 8,
+    marginBottom: 14,
+  },
+  image: {
+    height: vs(150),
+    width: "100%",
+    borderRadius: 10,
+  },
+});
