cee9d285d67e34e2caccbe1cc591d26c169fb6fb
Sourav Banerjee <sourav.banerjeejobs@gmail.com>
2026-05-16 19:37:23 +0530

    using reanimated for smoother movie poster loading

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -5,10 +5,28 @@ import { Movie } from "@/types";
 import { default_image } from "@/utils/assets";
 import { getGenreString } from "@/utils/genre";
 import { useRouter } from "expo-router";
+import { useState } from "react";
 import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from "react-native-reanimated";
 
 const MovieCard = ({ movie }: { movie: Movie }) => {
+  const [imageLoading, setImageLoading] = useState(true);
   const router = useRouter();
+  const opacityValue = useSharedValue(0);
+
+  const handleImageLoad = () => {
+    setImageLoading(false);
+    opacityValue.value = withTiming(1, { duration: 400 });
+  };
+
+  const animatedStyle = useAnimatedStyle(() => ({
+    opacity: opacityValue.value,
+  }));
+
   const tmdb_image_path = movie
     ? `${TMDB_IMAGE_BASE_PATH}${movie?.poster_path}`
     : "";
@@ -30,9 +48,18 @@ const MovieCard = ({ movie }: { movie: Movie }) => {
       activeOpacity={0.8}
       style={styles.container}
     >
+      {imageLoading ? (
+        <Image source={default_image} style={styles.image} />
+      ) : (
+        <Animated.Image
+          source={tmdb_image_path ? { uri: tmdb_image_path } : default_image}
+          style={[styles.image, animatedStyle]}
+        />
+      )}
       <Image
         source={tmdb_image_path ? { uri: tmdb_image_path } : default_image}
-        style={styles.image}
+        style={[styles.image, { position: "absolute", opacity: 0 }]}
+        onLoad={handleImageLoad}
       />
       <View>
         <Text
